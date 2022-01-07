12091694c736ce302cb8cd222ca0c956f71d1344
siciliad <david.siciliano@gmail.com>
2022-01-07 14:37:01 +0800

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,34 @@
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
+
+def deploy_fund_me():
+    account = get_account()
+
+    # If we are on a persistant network like Rinkeby use the associated address.
+    # Otherwise deploy mocks which is a local development chain.
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        # The following assignment MockV3Aggregator[-1].address references the most recently deployed mock V3 aggregator.
+        price_feed_address = MockV3Aggregator[-1].address
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print("Contract deployed to: " + fund_me.address)
+
+    return fund_me
+
+
+def main():
+    deploy_fund_me()
